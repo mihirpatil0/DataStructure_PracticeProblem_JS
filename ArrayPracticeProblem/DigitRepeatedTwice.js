30a143c9c4ef0af5857b13e2d38f71e1cfd7ecb0
mihirpatil0 <mihirrpatil@gmail.com>
2021-08-05 23:52:03 +0530

    [mihir] Added: storing double digit repeated numbers.

---
+++ ArrayPracticeProblem/DigitRepeatedTwice.js
@@ -0,0 +1,11 @@
+let number = new Array();
+
+for (let i = 1; i < 100; i++)
+{
+    if(i % 11 == 0)
+    {
+        number.push(i);
+    }
+}
+    
+console.log("Double digit repeated numbers are : " + number);
